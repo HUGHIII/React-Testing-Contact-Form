821118f82719a22c1394facdb5f0c47aef8ac6c6
TREY <hugh.a.lloyd@gmail.com>
2020-02-14 00:39:22 -0600

    need to fix test on input value

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,32 @@ import { render } from "@testing-library/react";
 import App from "./App";
 //arrange, act, assert
 
-test("simple test for text", () => {
- const { getByText } = render(<App />);
-
- const fName = getByText(/first name/i);
-
- expect(fName).toBeInTheDocument();
+test("renders App without crashing", () => {
+  render(<App />);
 });
+
+
+test("simple test for text", () => {
+  const { getByText } = render(<App />);
+ 
+  const fName = getByText(/first name/i);
+ 
+  expect(fName).toBeInTheDocument();
+  expect(fName).toBeTruthy()
+ });
+ 
+//  test("email type", () => {
+//   const { getByDisplayValue } = render(<App/>);
+ 
+//   const etype = getByDisplayValue(email);
+ 
+//   expect(etype).toHaveFormValue();
+//  });
+
+// test('inputs are rendered-firstname, last, email, and message ', () => {
+//     const {getByLabelText} =render(<App/>);
+//     getByLabelText(/first name/i);
+//     getByLabelText(/last name/i);
+//     getByLabelText(/email/i);
+//     getByLabelText(/message/i);
+// })
